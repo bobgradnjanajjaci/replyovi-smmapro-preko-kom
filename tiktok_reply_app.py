3a8a5e6fc12e5b31e1c4e80c4bb0cfed65e963c5
bobgradnjanajjaci <richsterius@gmail.com>
2026-06-17 01:24:12 +0200

    Update tiktok_reply_app.py

--- tiktok_reply_app.py
+++ tiktok_reply_app.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 
 # 🔧 KONFIGURACIJA – OVO POPUNI
 
-PANEL_URL = "https://smmapro.com/api/v2"
+PANEL_URL = "https://dnoxsmm.com/api/v2"
 
-API_KEY = "dc1ab06ff6035c6820503ad5fb191284"          # npr. 6b4b33ef856ee4ef973fcaa450e62786
+API_KEY = "fce3f72e2096a5472c2583434b8eee32"          # npr. 6b4b33ef856ee4ef973fcaa450e62786
 
-SERVICE_ID = 3279                       # STAVI TAČAN service ID za TikTok custom comment reply
+SERVICE_ID = 4575                       # STAVI TAČAN service ID za TikTok custom comment reply
 
 
 

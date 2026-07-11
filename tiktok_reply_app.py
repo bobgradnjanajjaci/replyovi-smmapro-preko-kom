e011c82b994e53b0843cd212cb86cbd66b41473e
bobgradnjanajjaci <richsterius@gmail.com>
2026-07-11 02:16:01 +0200

    Update tiktok_reply_app.py

--- tiktok_reply_app.py
+++ tiktok_reply_app.py
@@ -26,7 +26,7 @@ SERVICE_ID = 3246
 
 REPLIES_SET_1 = [
 "The craziest part is that none of this felt complicated once someone explained it properly. I honestly don’t know why nobody teaches AI like this.",
-"Glad someone else mentioned it. It genuinely helped me get my first online income. I’m surprised more people aren’t talking about it yet.","
+"Glad someone else mentioned it. It genuinely helped me get my first online income. I’m surprised more people aren’t talking about it yet.",
 "AMAZON",
 "AMAZON",
 "AMAZON",          
